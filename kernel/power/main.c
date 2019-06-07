36af88af85c3bd8bd0c109aa54b7d6890dc19923
Andrea Righi <andrea.righi@canonical.com>
2019-06-07 14:23:38 +0000

    UBUNTU SAUCE [aws]: PM / hibernate: make sure pm_async is always disabled
    BugLink: https://bugs.launchpad.net/bugs/1831940
    
    We have experienced deadlock conditions on hibernate under memory
    pressure with pm_async enabled.
    
    To prevent such deadlocks make sure that pm_async is never enabled.
    
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>
    Acked-by: Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Acked-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -94,7 +94,7 @@ int pm_notifier_call_chain(unsigned long val)
 }
 
 /* If set, devices may be suspended and resumed asynchronously. */
-int pm_async_enabled = 1;
+int pm_async_enabled;
 
 static ssize_t pm_async_show(struct kobject *kobj, struct kobj_attribute *attr,
 			     char *buf)
@@ -113,7 +113,7 @@ static ssize_t pm_async_store(struct kobject *kobj, struct kobj_attribute *attr,
 	if (val > 1)
 		return -EINVAL;
 
-	pm_async_enabled = val;
+	pm_async_enabled = 0;
 	return n;
 }
 
