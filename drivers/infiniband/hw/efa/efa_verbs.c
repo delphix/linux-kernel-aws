09654048cae0aff38dd0bd2dce6ced34b8d11361
Gal Pressman <galpress@amazon.com>
2019-06-23 13:21:41 +0300

    UBUNTU: SAUCE: linux/efa: Fix modify QP udata check backport
    BugLink: https://bugs.launchpad.net/bugs/1837638
    
    Modify QP needs the udata check WA, otherwise the udata validations fail
    on older kernels.
    
    Reviewed-by: Firas JahJah <firasj@amazon.com>
    Reviewed-by: Yossi Leybovich <sleybo@amazon.com>
    Signed-off-by: Gal Pressman <galpress@amazon.com>
    Reference: https://github.com/amzn/amzn-drivers/tree/master/kernel/linux/efa
    Signed-off-by: Connor Kuehl <connor.kuehl@canonical.com>
    Acked-by: Khalid Elmously <khalid.elmously@canonical.com>
    Acked-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- drivers/infiniband/hw/efa/efa_verbs.c
+++ drivers/infiniband/hw/efa/efa_verbs.c
@@ -5,6 +5,7 @@
 
 #include <linux/vmalloc.h>
 #include <linux/log2.h>
+#include <linux/version.h>
 
 #include <rdma/ib_addr.h>
 #include <rdma/ib_umem.h>
@@ -920,7 +921,12 @@ int efa_modify_qp(struct ib_qp *ibqp, struct ib_qp_attr *qp_attr,
 		return -EOPNOTSUPP;
 
 	if (udata->inlen &&
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,14,0)
 	    !ib_is_udata_cleared(udata, 0, udata->inlen)) {
+#else
+	    /* WA for e093111ddb6c ("IB/core: Fix input len in multiple user verbs") */
+	    !ib_is_udata_cleared(udata, 0, udata->inlen - sizeof(struct ib_uverbs_cmd_hdr))) {
+#endif
 		ibdev_dbg(&dev->ibdev,
 			  "Incompatible ABI params, udata not cleared\n");
 		return -EINVAL;
