41c2e2597e79c5a6c11c44fab0b0238b42a9bd68
Dominik Brodowski <linux@dominikbrodowski.net>
2019-10-29 18:37:52 +0100

    efi/random: Treat EFI_RNG_PROTOCOL output as bootloader randomness
    BugLink: https://bugs.launchpad.net/bugs/1859192
    
    Commit 428826f5358c ("fdt: add support for rng-seed") introduced
    add_bootloader_randomness(), permitting randomness provided by the
    bootloader or firmware to be credited as entropy. However, the fact
    that the UEFI support code was already wired into the RNG subsystem
    via a call to add_device_randomness() was overlooked, and so it was
    not converted at the same time.
    
    Note that this UEFI (v2.4 or newer) feature is currently only
    implemented for EFI stub booting on ARM, and further note that
    CONFIG_RANDOM_TRUST_BOOTLOADER must be enabled, and this should be
    done only if there indeed is sufficient trust in the bootloader
    _and_ its source of randomness.
    
    [ ardb: update commit log ]
    
    Tested-by: Bhupesh Sharma <bhsharma@redhat.com>
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-efi@vger.kernel.org
    Link: https://lkml.kernel.org/r/20191029173755.27149-4-ardb@kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit 18b915ac6b0ac5ba7ded03156860f60a9f16df2b)
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Connor Kuehl <connor.kuehl@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- drivers/firmware/efi/efi.c
+++ drivers/firmware/efi/efi.c
@@ -568,7 +568,7 @@ int __init efi_config_parse_tables(void *config_tables, int count, int sz,
 					      sizeof(*seed) + size);
 			if (seed != NULL) {
 				pr_notice("seeding entropy pool\n");
-				add_device_randomness(seed->bits, size);
+				add_bootloader_randomness(seed->bits, size);
 				early_memunmap(seed, sizeof(*seed) + size);
 			} else {
 				pr_err("Could not map UEFI random seed!\n");
