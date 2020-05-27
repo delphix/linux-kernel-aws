83c8008f878fa7ea01964f29830ac3896410d8f6
Andrea Righi <andrea.righi@canonical.com>
2020-05-27 10:49:32 +0200

    Revert "UBUNTU: SAUCE: xenbus: add freeze/thaw/restore callbacks support"
    BugLink: https://bugs.launchpad.net/bugs/1881869
    
    This reverts commit 05b40eec892e38be60184dfe1ca3590aceff741c. Superseded
    by newer Xen hibernation patch set.
    
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- drivers/xen/xenbus/xenbus_probe.c
+++ drivers/xen/xenbus/xenbus_probe.c
@@ -49,7 +49,6 @@
 #include <linux/io.h>
 #include <linux/slab.h>
 #include <linux/module.h>
-#include <linux/suspend.h>
 
 #include <asm/page.h>
 #include <asm/pgtable.h>
@@ -598,47 +597,26 @@ int xenbus_dev_suspend(struct device *dev)
 	struct xenbus_driver *drv;
 	struct xenbus_device *xdev
 		= container_of(dev, struct xenbus_device, dev);
-	int (*cb)(struct xenbus_device *) = NULL;
-	bool xen_suspend = xen_suspend_mode_is_xen_suspend();
 
 	DPRINTK("%s", xdev->nodename);
 
 	if (dev->driver == NULL)
 		return 0;
 	drv = to_xenbus_driver(dev->driver);
-
-	if (xen_suspend)
-		cb = drv->suspend;
-	else
-		cb = drv->freeze;
-
-	if (cb)
-		err = cb(xdev);
-
-	if (err) {
-		pr_warn("%s %s failed: %i\n", xen_suspend ?
-			"suspend" : "freeze", dev_name(dev), err);
-		return err;
-	}
-
-	if (!xen_suspend) {
-		/* Forget otherend since this can become stale after restore */
-		free_otherend_watch(xdev);
-		free_otherend_details(xdev);
-	}
-
+	if (drv->suspend)
+		err = drv->suspend(xdev);
+	if (err)
+		pr_warn("suspend %s failed: %i\n", dev_name(dev), err);
 	return 0;
 }
 EXPORT_SYMBOL_GPL(xenbus_dev_suspend);
 
 int xenbus_dev_resume(struct device *dev)
 {
-	int err = 0;
+	int err;
 	struct xenbus_driver *drv;
 	struct xenbus_device *xdev
 		= container_of(dev, struct xenbus_device, dev);
-	int (*cb)(struct xenbus_device *) = NULL;
-	bool xen_suspend = xen_suspend_mode_is_xen_suspend();
 
 	DPRINTK("%s", xdev->nodename);
 
@@ -647,34 +625,24 @@ int xenbus_dev_resume(struct device *dev)
 	drv = to_xenbus_driver(dev->driver);
 	err = talk_to_otherend(xdev);
 	if (err) {
-		pr_warn("%s (talk_to_otherend) %s failed: %i\n",
-			xen_suspend ? "resume" : "restore",
+		pr_warn("resume (talk_to_otherend) %s failed: %i\n",
 			dev_name(dev), err);
 		return err;
 	}
 
-	if (xen_suspend)
-		xdev->state = XenbusStateInitialising;
+	xdev->state = XenbusStateInitialising;
 
-	if (xen_suspend)
-		cb = drv->resume;
-	else
-		cb = drv->restore;
-
-	if (cb)
-		err = cb(xdev);
-
-	if (err) {
-		pr_warn("%s %s failed: %i\n",
-			xen_suspend ? "resume" : "restore",
-			dev_name(dev), err);
-		return err;
+	if (drv->resume) {
+		err = drv->resume(xdev);
+		if (err) {
+			pr_warn("resume %s failed: %i\n", dev_name(dev), err);
+			return err;
+		}
 	}
 
 	err = watch_otherend(xdev);
 	if (err) {
-		pr_warn("%s (watch_otherend) %s failed: %d.\n",
-			xen_suspend ? "resume" : "restore",
+		pr_warn("resume (watch_otherend) %s failed: %d.\n",
 			dev_name(dev), err);
 		return err;
 	}
@@ -685,46 +653,8 @@ EXPORT_SYMBOL_GPL(xenbus_dev_resume);
 
 int xenbus_dev_cancel(struct device *dev)
 {
-	int err = 0;
-	struct xenbus_driver *drv;
-	struct xenbus_device *xdev
-		= container_of(dev, struct xenbus_device, dev);
-	bool xen_suspend = xen_suspend_mode_is_xen_suspend();
-
-	if (xen_suspend) {
-		/* Do nothing */
-		DPRINTK("cancel");
-		return 0;
-	}
-
-	DPRINTK("%s", xdev->nodename);
-
-	if (dev->driver == NULL)
-		return 0;
-	drv = to_xenbus_driver(dev->driver);
-
-	err = talk_to_otherend(xdev);
-	if (err) {
-		pr_warn("thaw (talk_to_otherend) %s failed: %d.\n",
-			dev_name(dev), err);
-		return err;
-	}
-
-	if (drv->thaw) {
-		err = drv->thaw(xdev);
-		if (err) {
-			pr_warn("thaw %s failed: %i\n", dev_name(dev), err);
-			return err;
-		}
-	}
-
-	err = watch_otherend(xdev);
-	if (err) {
-		pr_warn("thaw (watch_otherend) %s failed: %d.\n",
-			dev_name(dev), err);
-		return err;
-	}
-
+	/* Do nothing */
+	DPRINTK("cancel");
 	return 0;
 }
 EXPORT_SYMBOL_GPL(xenbus_dev_cancel);
