6f96ca3c0b9bbb056466cbf992501c2f18ec9995
Andrea Righi <andrea.righi@canonical.com>
2020-12-04 17:12:18 +0100

    UBUNTU: SAUCE: xen-netfront: prevent unnecessary close on hibernate
    BugLink: https://bugs.launchpad.net/bugs/1906850
    
    If the device in the Xen bus is already in the "closed" state when
    hibernating there's no need to close the bus again. Doing so can only
    cause errors that would prevent the system to hibernate correctly.
    
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>
    Acked-by: William Breathitt Gray <william.gray@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Ian May <ian.may@canonical.com>

--- drivers/net/xen-netfront.c
+++ drivers/net/xen-netfront.c
@@ -2023,6 +2023,9 @@ static int netfront_freeze(struct xenbus_device *dev)
 
 	netif_device_detach(info->netdev);
 
+	if (dev->state == XenbusStateClosed)
+		goto closed;
+
 	info->freeze_state = NETIF_FREEZE_STATE_FREEZING;
 
 	/* Kick the backend to disconnect */
@@ -2039,7 +2042,7 @@ static int netfront_freeze(struct xenbus_device *dev)
 				 "the device may become inconsistent state");
 		return err;
 	}
-
+closed:
 	/* Tear down queues */
 	xennet_disconnect_backend(info);
 	xennet_destroy_queues(info);
