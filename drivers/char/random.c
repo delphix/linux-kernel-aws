700cd7d44e571c20a5862c581b045a828334aebb
Andy Lutomirski <luto@kernel.org>
2021-05-07 10:15:43 +0200

    random: add GRND_INSECURE to return best-effort non-cryptographic bytes
    BugLink: https://bugs.launchpad.net/bugs/1927692
    
    Signed-off-by: Andy Lutomirski <luto@kernel.org>
    Link: https://lore.kernel.org/r/d5473b56cf1fa900ca4bd2b3fc1e5b8874399919.1577088521.git.luto@kernel.org
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    (backported from commit 75551dbf112c992bc6c99a972990b3f272247e23)
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>
    Acked-by: Guilherme Piccoli <gpiccoli@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -2207,7 +2207,14 @@ SYSCALL_DEFINE3(getrandom, char __user *, buf, size_t, count,
 {
 	int ret;
 
-	if (flags & ~(GRND_NONBLOCK|GRND_RANDOM))
+	if (flags & ~(GRND_NONBLOCK|GRND_RANDOM|GRND_INSECURE))
+		return -EINVAL;
+
+	/*
+	 * Requesting insecure and blocking randomness at the same time makes
+	 * no sense.
+	 */
+	if ((flags & (GRND_INSECURE|GRND_RANDOM)) == (GRND_INSECURE|GRND_RANDOM))
 		return -EINVAL;
 
 	if (count > INT_MAX)
@@ -2216,7 +2223,7 @@ SYSCALL_DEFINE3(getrandom, char __user *, buf, size_t, count,
 	if (flags & GRND_RANDOM)
 		return _random_read(flags & GRND_NONBLOCK, buf, count);
 
-	if (crng_init == 0) {
+	if (!(flags & GRND_INSECURE) && !crng_ready()) {
 		if (flags & GRND_NONBLOCK)
 			return -EAGAIN;
 		ret = wait_event_interruptible(crng_init_wait, crng_init > 0);
