ce2ff135cb3a77c5418b561fabc851f61c687873
David Woodhouse <dwmw@amazon.co.uk>
2021-11-08 16:09:41 +0000

    UBUNTU: SAUCE: PM: hibernate: Allow ACPI hardware signature to be honoured
    BugLink: https://bugs.launchpad.net/bugs/2060992
    
    Theoretically, when the hardware signature in FACS changes, the OS
    is supposed to gracefully decline to attempt to resume from S4:
    
     "If the signature has changed, OSPM will not restore the system
      context and can boot from scratch"
    
    In practice, Windows doesn't do this and many laptop vendors do allow
    the signature to change especially when docking/undocking, so it would
    be a bad idea to simply comply with the specification by default in the
    general case.
    
    However, there are use cases where we do want the compliant behaviour
    and we know it's safe. Specifically, when resuming virtual machines where
    we know the hypervisor has changed sufficiently that resume will fail.
    We really want to be able to *tell* the guest kernel not to try, so it
    boots cleanly and doesn't just crash. This patch provides a way to opt
    in to the spec-compliant behaviour on the command line.
    
    A follow-up patch may do this automatically for certain "known good"
    machines based on a DMI match, or perhaps just for all hypervisor
    guests since there's no good reason a hypervisor would change the
    hardware_signature that it exposes to guests *unless* it wants them
    to obey the ACPI specification.
    
    Signed-off-by: David Woodhouse <dwmw@amazon.co.uk>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 74d9555580c48a04b2c3b742dfb0c80777aa0b26)
    (backported from commit 9bb5343dc480ac00683d8d8e982cbef124ec32a3 from git.infradead.org/users/dwmw2/linux.git/psci-hibernate-5.15
    [calling conventions in acpi_sleep_setup adjusted])
    Signed-off-by: Philip Cox <philip.cox@canonical.com>
    Acked-by: John Cabaj <john.cabaj@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>

--- arch/x86/kernel/acpi/sleep.c
+++ arch/x86/kernel/acpi/sleep.c
@@ -141,9 +141,9 @@ static int __init acpi_sleep_setup(char *str)
 			acpi_realmode_flags |= 4;
 #ifdef CONFIG_HIBERNATION
 		if (strncmp(str, "s4_hwsig", 8) == 0)
-			acpi_check_s4_hw_signature = 1;
+			acpi_check_s4_hw_signature(1);
 		if (strncmp(str, "s4_nohwsig", 10) == 0)
-			acpi_check_s4_hw_signature = 0;
+			acpi_check_s4_hw_signature(0);
 #endif
 		if (strncmp(str, "nonvs", 5) == 0)
 			acpi_nvs_nosave();
