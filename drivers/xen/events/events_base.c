c3fa81558d339cdc567ed258b75aa7b0f64b3839
Frank van der Linden <fllinden@amazon.com>
2022-08-17 16:51:47 +0800

    UBUNTU: SAUCE: HIBERNATION: xen: restore pirqs on resume from hibernation.
    BugLink: https://bugs.launchpad.net/bugs/1968062
    
    The hibernation code unlinks event channels from these (legacy) IRQs, so they
    must be reinitialized on wakeup, much like in the Xen suspend/resume case.
    
    Signed-off-by: Frank van der Linden <fllinden@amazon.com>
    Reviewed-by: Cristian Gafton <gafton@amazon.com>
    Reviewed-by: Anchal Agarwal <anchalag@amazon.com>
    Reviewed-by: Alakesh Haloi <alakeshh@amazon.com>
    
    CR: https://code.amazon.com/reviews/CR-3702953/
    (cherry picked from commit 5ac2dbdedcf419c726f323cd4c2f93ff652b46b4 amazon-5.15.y/mainline)
    Signed-off-by: Gerald Yang <gerald.yang@canonical.com>
    Signed-off-by: Matthew Ruffell <matthew.ruffell@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Marcelo Cerri <marcelo.cerri@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/xen/events/events_base.c
+++ drivers/xen/events/events_base.c
@@ -2153,6 +2153,11 @@ void xen_shutdown_pirqs(void)
 	}
 }
 
+void xen_restore_pirqs(void)
+{
+	restore_pirqs();
+}
+
 static struct irq_chip xen_dynamic_chip __read_mostly = {
 	.name			= "xen-dyn",
 
