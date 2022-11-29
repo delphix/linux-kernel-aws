86c7dec0a069fe62d5f66c6d913f74ad49f00604
Paolo Pisati <paolo.pisati@canonical.com>
2022-11-29 16:29:54 +0000

    UBUNTU: SAUCE: fixup lock_system_sleep()/unlock_system_sleep()
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- drivers/xen/manage.c
+++ drivers/xen/manage.c
@@ -118,9 +118,10 @@ static int xen_suspend(void *data)
 static void do_suspend(void)
 {
 	int err;
+	unsigned int sleep_flags;
 	struct suspend_info si;
 
-	lock_system_sleep();
+	sleep_flags = lock_system_sleep();
 
 	suspend_mode = XEN_SUSPEND;
 
@@ -199,7 +200,7 @@ out:
 
 	suspend_mode = NO_SUSPEND;
 
-	unlock_system_sleep();
+	unlock_system_sleep(sleep_flags);
 }
 #endif	/* CONFIG_HIBERNATE_CALLBACKS */
 
