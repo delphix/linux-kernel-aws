7bfa8739b027b8e9302b8d5d091200cafef61c2e
Chengen Du <chengen.du@canonical.com>
2023-01-18 15:57:00 +0100

    NFS: Judge the file access cache's timestamp in rcu path
    BugLink: https://bugs.launchpad.net/bugs/2003053
    
    If the user's login time is newer than the cache's timestamp,
    we expect the cache may be stale and need to clear.
    The stale cache will remain in the list's tail if no other
    users operate on that inode.
    Once the user accesses the inode, the stale cache will be
    returned in rcu path.
    
    Signed-off-by: Chengen Du <chengen.du@canonical.com>
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    
    (cherry picked from commit 029085b8949f5d269ae2bbd14915407dd0c7f902)
    Signed-off-by: Chengen Du <chengen.du@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Luke Nowakowski-Krijger <luke.nowakowskikrijger@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- fs/nfs/dir.c
+++ fs/nfs/dir.c
@@ -2421,6 +2421,7 @@ static int nfs_access_get_cached_rcu(struct inode *inode, const struct cred *cre
 	 * but do it without locking.
 	 */
 	struct nfs_inode *nfsi = NFS_I(inode);
+	u64 login_time = nfs_access_login_time(current, cred);
 	struct nfs_access_entry *cache;
 	int err = -ECHILD;
 	struct list_head *lh;
@@ -2434,6 +2435,8 @@ static int nfs_access_get_cached_rcu(struct inode *inode, const struct cred *cre
 	    cred != cache->cred)
 		cache = NULL;
 	if (cache == NULL)
+		goto out;
+	if ((s64)(login_time - cache->timestamp) > 0)
 		goto out;
 	if (nfs_check_cache_invalid(inode, NFS_INO_INVALID_ACCESS))
 		goto out;
