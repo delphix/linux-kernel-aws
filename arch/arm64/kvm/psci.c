80cf358f5d3f5ed02607cae4d7091a908e3004a0
David Woodhouse <dwmw@amazon.co.uk>
2024-03-11 08:02:16 +0000

    UBUNTU: SAUCE: KVM: arm64: Add PSCI v1.3 SYSTEM_OFF2 function for hibernation
    BugLink: https://bugs.launchpad.net/bugs/2060992
    
    The PSCI v1.3 specification (alpha) adds support for a SYSTEM_OFF2 function
    which is analogous to ACPI S4 state. This will allow hosting environments
    to determine that a guest is hibernated rather than just powered off, and
    ensure that they preserve the virtual environment appropriately to allow
    the guest to resume safely (or bump the hardware_signature in the FACS to
    trigger a clean reboot instead).
    
    The beta version will be changed to say that PSCI_FEATURES returns a bit
    mask of the supported hibernate types, which is implemented here.
    
    Although this new feature is inflicted unconditionally on unexpecting
    userspace, it ought to be mostly OK because it still results in the same
    KVM_SYSTEM_EVENT_SHUTDOWN event, just with a new flag which hopefully
    won't cause userspace to get unhappy.
    
    Signed-off-by: David Woodhouse <dwmw@amazon.co.uk>
    (cherry picked from commit 3cad88da46c6f1f08a45aaa2201e8b2dcfda3875 from https://git.infradead.org/users/dwmw2/linux.git/shortlog/refs/heads/psci-hibernate-6.8)
    Signed-off-by: Philip Cox <philip.cox@canonical.com>
    Acked-by: John Cabaj <john.cabaj@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>

--- arch/arm64/kvm/psci.c
+++ arch/arm64/kvm/psci.c
@@ -194,6 +194,12 @@ static void kvm_psci_system_off(struct kvm_vcpu *vcpu)
 	kvm_prepare_system_event(vcpu, KVM_SYSTEM_EVENT_SHUTDOWN, 0);
 }
 
+static void kvm_psci_system_off2(struct kvm_vcpu *vcpu)
+{
+	kvm_prepare_system_event(vcpu, KVM_SYSTEM_EVENT_SHUTDOWN,
+				 KVM_SYSTEM_EVENT_SHUTDOWN_FLAG_PSCI_OFF2);
+}
+
 static void kvm_psci_system_reset(struct kvm_vcpu *vcpu)
 {
 	kvm_prepare_system_event(vcpu, KVM_SYSTEM_EVENT_RESET, 0);
@@ -358,6 +364,11 @@ static int kvm_psci_1_x_call(struct kvm_vcpu *vcpu, u32 minor)
 			if (minor >= 1)
 				val = 0;
 			break;
+		case PSCI_1_3_FN_SYSTEM_OFF2:
+		case PSCI_1_3_FN64_SYSTEM_OFF2:
+			if (minor >= 3)
+				val = BIT(PSCI_1_3_HIBERNATE_TYPE_OFF);
+			break;
 		}
 		break;
 	case PSCI_1_0_FN_SYSTEM_SUSPEND:
@@ -391,6 +402,32 @@ static int kvm_psci_1_x_call(struct kvm_vcpu *vcpu, u32 minor)
 			val = PSCI_RET_INVALID_PARAMS;
 			break;
 		}
+		break;
+	case PSCI_1_3_FN_SYSTEM_OFF2:
+		kvm_psci_narrow_to_32bit(vcpu);
+		fallthrough;
+	case PSCI_1_3_FN64_SYSTEM_OFF2:
+		if (minor < 3)
+			break;
+
+		arg = smccc_get_arg1(vcpu);
+		if (arg != PSCI_1_3_HIBERNATE_TYPE_OFF) {
+			val = PSCI_RET_INVALID_PARAMS;
+			break;
+		}
+		kvm_psci_system_off2(vcpu);
+		/*
+		 * We shouldn't be going back to guest VCPU after
+		 * receiving SYSTEM_OFF2 request.
+		 *
+		 * If user space accidentally/deliberately resumes
+		 * guest VCPU after SYSTEM_OFF2 request then guest
+		 * VCPU should see internal failure from PSCI return
+		 * value. To achieve this, we preload r0 (or x0) with
+		 * PSCI return value INTERNAL_FAILURE.
+		 */
+		val = PSCI_RET_INTERNAL_FAILURE;
+		ret = 0;
 		break;
 	default:
 		return kvm_psci_0_2_call(vcpu);
