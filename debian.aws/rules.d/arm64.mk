11f9e86c891e2d56d939e789d8c7ad0c042e7310
Paolo Pisati <paolo.pisati@canonical.com>
2025-01-28 10:54:30 +0100

    UBUNTU: [Packaging] rules.d: drop do_source_package
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- debian.aws/rules.d/arm64.mk
+++ debian.aws/rules.d/arm64.mk
@@ -19,7 +19,6 @@ do_extras_package = true
 ship_extras_package = true
 do_zfs         = true
 disable_d_i    = true
-do_source_package = false
 do_dtbs                = false
 do_common_headers_indep = false
 do_tools_perf_jvmti = true
