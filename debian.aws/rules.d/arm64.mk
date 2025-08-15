dd7e906dd15ee40c09e89e50eb275c4d3fb163cf
Paolo Pisati <paolo.pisati@canonical.com>
2025-08-15 09:08:13 +0200

    UBUNTU: [Packaging] remove do_extras_package
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- debian.aws/rules.d/arm64.mk
+++ debian.aws/rules.d/arm64.mk
@@ -15,7 +15,6 @@ do_tools_perf   = true
 do_tools_x86   = false
 do_tools_bpftool = false
 do_tools_hyperv        = true
-do_extras_package = true
 ship_extras_package = true
 do_zfs         = true
 disable_d_i    = true
